c73ea35d6f98862fecc436f9c504d9ae38fac2e2
Samie <samrasolomon40@gmail.com>
2022-03-22 21:41:51 +0000

    Added a file

--- models/base_model.py
+++ models/base_model.py
@@ -16,17 +16,11 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
+        for key, value in kwargs.items():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
--- models/place.py
+++ models/place.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 import os
+
+
+for_relationship = Table('place_amenity', Base.metadata,
+                         Column('place_id', String(60),
+                                ForeignKey('places.id'), primary_key=True,
+                                nullable=False),
+                         Column('amenity_id', String(60),
+                                ForeignKey('amenities.id'), primary_key=True,
+                                nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -20,16 +29,42 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", cascade="all, delete", backref="place")
+    amenities = relationship("Amenity", secondary=for_relationship,
+                             viewonly=False)
+    amenity_ids = []
 
     if (os.getenv('HBNB_TYPE_STORAGE') != 'db'):
+        @property
         def reviews(self):
             """This function returns the list of Review instances with
                place_id equals to the current Place.id
             """
             from models import storage
-            from models.city import Review
+            from models.review import Review
             new_list = []
-            for key, value in storage.all(Review):
+            for key, value in storage.all(Review).items():
                 if (value.id == self.id):
                     new_list.append(value)
             return new_list
+
+        @property
+        def amenities(self):
+            """returns the list of Amenity instances based on the attribute
+               amenity_ids that contains all Amenity.id linked to the Place
+            """
+            from models import storage
+            from models.amenity import Amenity
+            new_list = []
+            for key, value in storage.all(Amenity).items():
+                for i in amenity_id:
+                    if (i == value.id):
+                        new_list.append(value)
+            return new_list
+
+        @amenities.setter
+        def amenities(self, obj):
+            """append method for adding an Amenity.id to the attribute
+               amenity_ids
+            """
+            if obj:
+                Place.amenity_ids.append(obj.id)
