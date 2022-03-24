dd3a3954f0fc1e31f4a35e5f66a85571fcbfe87d
Samie <samrasolomon40@gmail.com>
2022-03-24 20:09:04 +0000

    Modified a file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 """This script will distribute archive in webserver"""
 
 from fabric.api import *
-from fabric.operations import run, put
+from fabric.operations import sudo, put
 import os
 
 env.hosts = ['ubuntu@34.148.148.119', 'ubuntu@44.200.78.83']
@@ -14,13 +14,16 @@ def do_deploy(archive_path):
         value = archive_path.split('/')[1]
         new = value.split(".")[0]
         put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}/".format(new))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(value, new))
-        run("rm /tmp/{}".format(value))
-        run("mv /data/web_static/releases/{}/web_static/* \
+        sudo("mkdir -p /data/web_static/releases/{}/".format(new))
+        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+             .format(value, new))
+        sudo("rm /tmp/{}".format(value))
+        sudo("mv /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/".format(new, new))
-        run("rm -rf /data/web_static/releases/{}/web_static/".format(new))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(new))
+        sudo("rm -rf /data/web_static/releases/{}/web_static/".format(new))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+             .format(new))
+        return True
+    else:
+        return False
