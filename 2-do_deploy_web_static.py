199ea47896c40f2e7d717f2d446d2c01c397df24
Samie <samrasolomon40@gmail.com>
2022-03-24 20:15:03 +0000

    Modified a file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,15 +14,14 @@ def do_deploy(archive_path):
         value = archive_path.split('/')[1]
         new = value.split(".")[0]
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(new))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+        run("mkdir -p /data/web_static/releases/{}/".format(new))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(value, new))
-        run("sudo rm /tmp/{}".format(value))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(new, new))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static/".format(new))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        run("rm /tmp/{}".format(value))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(new, new))
+        run("rm -rf /data/web_static/releases/{}/web_static/".format(new))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(new))
         return True
     else:
