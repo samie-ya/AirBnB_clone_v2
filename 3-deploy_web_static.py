f035fe94b03ba01c1870b6a453dded30fb158abd
Samie <samrasolomon40@gmail.com>
2022-03-24 20:45:47 +0000

    Added a file

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """This function will depoly all the other functions"""
 
+from fabric.api import env
 from 1-pack_web_static import do_pack
 from 2-do_deploy_web_static import do_deploy
 
 env.hosts = ['ubuntu@34.148.148.119', 'ubuntu@44.200.78.83']
+
 
 def deploy():
     """This will sum up the other functions we created"""
     pack = do_pack()
     if not pack:
         return False
-    return do_deploy(pack)
+    return do_dep(pack)
