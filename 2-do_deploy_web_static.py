30c0bbe11fb23d3a052be38782ea22f78aa4f961
Samie <samrasolomon40@gmail.com>
2022-03-25 22:51:19 +0000

    Modified a file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,10 +10,8 @@ env.hosts = ['ubuntu@34.148.148.119', 'ubuntu@44.200.78.83']
 
 def do_deploy(archive_path):
     """This function will depoly archives"""
-    if not os.path.exists(archive_path):
-        return False
-    else:
-        value = archive_path.split('/')[1]
+    if os.path.exists(archive_path):
+        value = archive_path.split("/")[1]
         new = value.split(".")[0]
         put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/{}/".format(new))
@@ -21,9 +19,11 @@ def do_deploy(archive_path):
             .format(value, new))
         run("rm /tmp/{}".format(value))
         run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(new, new))
-        run("rm -rf /data/web_static/releases/{}/web_static/".format(new))
+             /data/web_static/releases/{}/".format(new, new))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(new))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(new))
         return True
+    else:
+        return False
