a1828feb6bcba735e5736dd0cb934d6e3bea4485
samie-ya <samrasolomon40@gmail.com>
2022-04-08 07:18:54 +0000

    Added files

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This script will create a simple flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
