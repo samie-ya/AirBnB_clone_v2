9a73135473cf9a15124d58ec2eaabef324cde81d
samie-ya <samrasolomon40@gmail.com>
2022-04-08 07:40:45 +0000

    Added a file

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This script will create two simple flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """This function will use url with slash and give the result"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """This function will redirect to hbnb and display return"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def with_C(text):
+    """This functio will display c followed by text value"""
+    return "C %s" % text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
