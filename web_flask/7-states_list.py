96dc6c8162ee0468da209d6ba8389ba9e279e750
samie-ya <samrasolomon40@gmail.com>
2022-04-09 17:23:57 +0000

    Modified a file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """This script will create 5 simple flask web application"""
-
 from models import storage
-from models.state import State
 from flask import Flask, render_template
 app = Flask(__name__)
-
-new_list = []
-for state_id, state in storage.all(State).items():
-    new_list.append(state)
 
 
 @app.route("/states_list", strict_slashes=False)
 def states():
     """This function will display html that conatins states"""
+    from models.state import State
+    new_list = []
+    for state_id, state in storage.all(State).items():
+        new_list.append(state)
     return render_template('7-states_list.html', name=new_list)
 
 
